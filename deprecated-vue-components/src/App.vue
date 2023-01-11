d0ab2ec310c671924b69f84067a271a950a7b716
Julian Müller <julian2.mueller@tum.de>
2023-01-11 16:18:58 +0100

    Add depr. vue version of generic histogram

--- deprecated-vue-components/src/App.vue
+++ deprecated-vue-components/src/App.vue
@@ -1,22 +1,13 @@
 <template>
   <div id="app">
-    <h1>Scatter plot</h1>
-    <scatter-plot
-        identifier1="gene_X"
-        identifier2="gene_Y"
-        :expressions1="[ { 'Sample name': 1, 'Z-score' : 3 }, { 'Sample name': 2, 'Z-score' : 1 }, { 'Sample name': 3, 'Z-score' : 3 } ]"
-        :expressions2="[ { 'Sample name': 1, 'Z-score' : 2 }, { 'Sample name': 2, 'Z-score' : 1 }, { 'Sample name': 3, 'Z-score' : 5 } ]"
-      />
   </div>
 </template>
 
 <script>
-import ScatterPlot from './components/ScatterPlot.vue'
 
 export default {
   name: 'App',
   components: {
-    ScatterPlot
   }
 }
 </script>
