ebd4fc03b03cab5e411e8356889c55915cd445b4
Julian Müller <julian2.mueller@tum.de>
2023-01-11 10:25:02 +0100

    Add styling for Download Button

--- packages/biowc-scatter/src/biowc-scatter.css.ts
+++ packages/biowc-scatter/src/biowc-scatter.css.ts
@@ -23,5 +23,16 @@ export default css`
   .btn {
     margin-left: 25px;
     height: 25px;
+    background-color: #207cb9;
+    border: none;
+    color: white;
+    padding: 3px 7px;
+    cursor: pointer;
+    font-size: 12px;
+    border-radius: 6px;
+  }
+
+  .btn:hover {
+    background-color: #175a81;
   }
 `;
