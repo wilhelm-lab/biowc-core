37706053cbce80d1fd18ce3c3e3439f9f4144fe8
Matthew The <matthew.the@tum.de>
2023-01-11 16:07:01 +0100

    Added deprecated spectrum viewer

---
+++ deprecated-vue-components/src/stories/fixtures/SpectrumViewerFixture.js
@@ -0,0 +1,199 @@
+export default {
+    spectrumViewer: {
+      plotdata: {
+        x: [123.12, 234.23],
+        y: [10.0, 100.0],
+        id: [0, 1],
+        color: ["#0d75bc", "#a6a6a6"],
+        label: ["b1", ""],
+        labelCharge: [1, 0],
+        neutralLosses: ["", ""],
+        barwidth: [3, 1],
+        massError: ["", 2.5],
+        theoMz: [0, 0],
+        percentBasePeak: [100, 50],
+        TIC: 110.0,
+        massErrorX: [123.12, 234.23],
+        intensityError: [5, 4],
+        intensityErrorIdsTop: [0, 1],
+        intensityErrorIdsBottom: [0, 1],
+      },
+      peptide: {
+        sequence: "TESTPEPTIDE",
+        precursorMz: 123.12,
+        precursorCharge: 2,
+        mods: [
+          {
+            site: -1,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 0,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 1,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 2,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 3,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 4,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 5,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 6,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 7,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 8,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 9,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 10,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 11,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+        ],
+        origin: "Experimental",
+      },
+      mirrorplotdata: {
+        x: [135.13, 234.25],
+        y: [100.0, 10.0],
+        id: [0, 1],
+        color: ["#a6a6a6", "#a6a6a6"],
+        label: ["", ""],
+        labelCharge: [0, 0],
+        neutralLosses: ["", ""],
+        barwidth: [1, 1],
+        massError: ["", ""],
+        theoMz: [0, 0],
+        percentBasePeak: [50, 100],
+        TIC: 110.0,
+      },
+      peptideBottom: {
+        sequence: "TESTPEPTIDE",
+        precursorMz: 123.12,
+        precursorCharge: 2,
+        mods: [
+          {
+            site: -1,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 0,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 1,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 2,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 3,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 4,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 5,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 6,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 7,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 8,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 9,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 10,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+          {
+            site: 11,
+            deltaElement: null,
+            deltaMass: 0,
+          },
+        ],
+        origin: "Origin",
+      },
+      settings: {
+        toleranceThreshold: 40,
+        toleranceType: "ppm",
+        ionizationMode: "+",
+      },
+      score: {
+        sa: 0.5,
+        corr: 0.6,
+      },
+      scoreTop: {
+        sa: 0.2,
+        corr: 0.3,
+      },
+      scoreBottom: {
+        sa: 0.4,
+        corr: 0.5,
+      }
+    }
+}
