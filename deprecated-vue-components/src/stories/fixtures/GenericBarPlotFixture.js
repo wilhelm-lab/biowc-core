552b5242331637529cbb847696e002fd31540ae2
Julian Müller <julian2.mueller@tum.de>
2023-01-12 14:28:16 +0100

    Finish deprecated GenericBarPlot Vue component

--- deprecated-vue-components/src/stories/fixtures/GenericBarPlotFixture.js
+++ deprecated-vue-components/src/stories/fixtures/GenericBarPlotFixture.js
@@ -8,15 +8,35 @@ const barplot = {
     AttributeType: 'Attribute Type',
     Dataset: 'Dataset Name',
     data: [{
+      ModelId: '0',
+      DrugId: 1,
+      CellLineId: 1,
       VALUE: 3.5,
-      ModelId: '0', //TODO: Can be multiple, ;-separated. I am so sorry...
-      DrugId: 'Exampletinib',
-      CellLineId: 'CellLine X',
-      MIN_VALUE: -10,
-      MAX_VALUE: 10,
-      CellLine: 'DoIHaveToSpecifyThis?',
-      Drug: 'Andwhataboutthis?'
-    }]
+      MIN_VALUE: 1,
+      MAX_VALUE: 5,
+      Drug: 'Exampletinib',
+      CellLine: 'Cellline A'
+    },
+      {
+        ModelId: '1',
+        DrugId: 2,
+        CellLineId: 3,
+        VALUE: 2,
+        MIN_VALUE: 4, //Here's a todo: It shouldn't be possible that the value range does not include the value
+        MAX_VALUE: 8,
+        Drug: 'Whateverimab',
+        CellLine: 'Cellline C'
+      },
+      {
+        ModelId: '2',
+        DrugId: 5,
+        CellLineId: 6,
+        VALUE: 4,
+        MIN_VALUE: 3.9,
+        MAX_VALUE: 4.1,
+        Drug: 'Foobarbazine',
+        CellLine: 'Cellline B'
+      }]
   },
   sSelectedModelIds: { data: [] } //list of 'modelIds'
 
