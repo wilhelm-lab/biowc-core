4f8bdc5cd19117761e4b32ad67e83f73a83e0dd8
Julian Müller <julian2.mueller@tum.de>
2023-01-17 09:55:01 +0100

    Implement test data and render it as text

--- packages/biowc-lineplot/test/biowc-lineplot.test.ts
+++ packages/biowc-lineplot/test/biowc-lineplot.test.ts
@@ -2,37 +2,20 @@ import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
 import { BiowcLineplot } from '../src/BiowcLineplot.js';
 import '../src/biowc-lineplot.js';
+import LinePlotFixture from './fixtures/LinePlotFixture.js';
 
 describe('BiowcLineplot', () => {
-  it('has a default mytitle "Hey there" and counter 5', async () => {
-    const el = await fixture<BiowcLineplot>(
-      html`<biowc-lineplot></biowc-lineplot>`
-    );
-
-    expect(el.mytitle).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
-  });
-
-  it('increases the counter on button click', async () => {
-    const el = await fixture<BiowcLineplot>(
-      html`<biowc-lineplot></biowc-lineplot>`
-    );
-    el.shadowRoot!.querySelector('button')!.click();
-
-    expect(el.counter).to.equal(6);
-  });
-
-  it('can override the mytitle via attribute', async () => {
-    const el = await fixture<BiowcLineplot>(
-      html`<biowc-lineplot mytitle="attribute mytitle"></biowc-lineplot>`
-    );
-
-    expect(el.mytitle).to.equal('attribute mytitle');
+  it('consists only of 2-dimensional points', () => {
+    expect(
+      LinePlotFixture.basicLineplot.dataPoints[0]
+        .map(elem => elem.length)
+        .every(len => len === 2)
+    ).to.be.true;
   });
 
   it('passes the a11y audit', async () => {
     const el = await fixture<BiowcLineplot>(
-      html`<biowc-lineplot></biowc-lineplot>`
+      html` <biowc-lineplot></biowc-lineplot>`
     );
 
     await expect(el).shadowDom.to.be.accessible();
---
+++ packages/biowc-lineplot/test/fixtures/LinePlotFixture.ts
@@ -0,0 +1,13 @@
+const basicLineplot = {
+  dataPoints: [
+    [
+      [0, 1],
+      [1, 2],
+      [2, 5],
+      [3, 4],
+      [4, 6],
+    ],
+  ],
+};
+
+export default { basicLineplot };
