70e9db33bd43933cbcc37772486d876f2ce84119
Heriette <la.heriette@gmail.com>
2023-01-18 18:24:45 +0100

    added tests

--- packages/biowc-violinplot/test/biowc-violinplot.test.ts
+++ packages/biowc-violinplot/test/biowc-violinplot.test.ts
@@ -1,5 +1,5 @@
 import { html } from 'lit';
-import { fixture, expect } from '@open-wc/testing';
+import { fixture, expect, aTimeout } from '@open-wc/testing';
 import { BiowcViolinplot } from '../src/BiowcViolinplot.js';
 import '../src/biowc-violinplot.js';
 import ViolinPlotFixture from './fixtures/ViolinPlotFixture.js';
@@ -28,7 +28,59 @@ describe('BiowcViolinplot', async () => {
     expect(el.height).to.equal(500);
   });
 
+  // change yAxisLabel
+  it('can change Y axis label via attribute', async () => {
+    const el = await fixture<BiowcViolinplot>(
+      html`<biowc-violinplot
+        .yAxisLabel=${'some random label'}
+      ></biowc-violinplot>`
+    );
+
+    expect(el.yAxisLabel).to.equal('some random label');
+  });
+
   it('contains 3 data entries', () => {
     expect(violinplot.chartData.length).to.equal(3);
+  });
+
+  // test if selecting d3 violin triggers a function which returns value
+  it('returns information about selected violin plot', async () => {
+    const box = violinplot.shadowRoot!.querySelector(`#violin-bbox-0`);
+    box?.addEventListener('select-violin', (e: any) =>
+      expect(e.detail.sampleName).to.equal(
+        ViolinPlotFixture.violinPlot.chartData[0].sampleName
+      )
+    );
+    box!.dispatchEvent(new MouseEvent('click'));
+    await aTimeout(500);
+  });
+});
+
+describe('BiowcViolinplot', async () => {
+  const someData = [
+    {
+      idKey: 99,
+      sampleName: 'unknown',
+      score: 100,
+      data: [
+        {
+          proteinId: 0,
+          geneName: 'unknown_gene',
+          value: 5,
+        },
+      ],
+    },
+  ];
+
+  const newViolinplot = await fixture<BiowcViolinplot>(
+    html` <biowc-violinplot
+      .plotLabelValueCatds="${ViolinPlotFixture.violinPlot.plotLabelValueCatds}"
+      .chartData="${someData}"
+      ,
+    />`
+  );
+
+  it('can run with custom data', async () => {
+    expect(newViolinplot.chartData.length).to.equal(1);
   });
 });
