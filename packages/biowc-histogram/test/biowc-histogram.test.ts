727db6df241f7bebab3a472589c9c233756d97b9
Matthew The <matthew.the@tum.de>
2024-03-12 17:31:33 +0100

    reorganized unit tests

--- packages/biowc-histogram/test/biowc-histogram.test.ts
+++ packages/biowc-histogram/test/biowc-histogram.test.ts
@@ -5,97 +5,178 @@ import HistogramFixture from './fixtures/HistogramFixture.js';
 
 window.customElements.define('biowc-histogram', BiowcHistogram);
 
-describe('BiowcHistogram', async () => {
-  const histogram = await fixture<BiowcHistogram>(
-    html` <biowc-histogram
-      .idKey="${HistogramFixture.histogram.idKey}"
-      .valueKey="${HistogramFixture.histogram.valueKey}"
-      .xLabel="${HistogramFixture.histogram.xLabel}"
-      .yLabel="${HistogramFixture.histogram.yLabel}"
-      .xValues="${HistogramFixture.histogram.xValues}"
-    />`
-  );
+// TODO: replace with checks on actual HTML elements instead of JS variables.
+describe('Default Component', () => {
+  let el: BiowcHistogram;
 
-  it('has empty x and y labels by default', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram></biowc-histogram>`
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html` <biowc-histogram></biowc-histogram> `
     );
+  });
 
+  it('has empty x and y labels', async () => {
     expect(el.xLabel).to.equal('');
     expect(el.yLabel).to.equal('');
   });
 
-  it('has empty x values by default', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram></biowc-histogram>`
-    );
-
+  it('has empty x values', async () => {
     expect(el.xValues.length).to.equal(0);
   });
 
-  it('has width 460 and height 400 by default', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram></biowc-histogram>`
-    );
-
+  it('has width 460 and height 400', async () => {
     expect(el.height).to.equal(400);
     expect(el.width).to.equal(460);
   });
 
-  it('can override height and width via attribute', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram .height=${200} .width=${300}></biowc-histogram>`
-    );
-
-    expect(el.height).to.equal(200);
-    expect(el.width).to.equal(300);
-  });
-
-  it('can override the x and y labels via attribute', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram
-        .xLabel=${'x label'}
-        .yLabel=${'y label'}
-      ></biowc-histogram>`
-    );
-
-    expect(el.xLabel).to.equal('x label');
-    expect(el.yLabel).to.equal('y label');
-  });
-
-  it('can override the x values via attribute', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram
-        .xValues=${[{ id: 1, value: 3 }]}
-      ></biowc-histogram>`
-    );
-
-    expect(el.xValues.length).to.equal(1);
-  });
-
   it('has 25 bins by default', async () => {
-    const el = await fixture<BiowcHistogram>(
-      html`<biowc-histogram></biowc-histogram>`
-    );
-
     expect(el.numBins).to.equal(25);
     // expect(el.bins.length).to.equal(25);
   });
 
-  it('can override the number of bins via attribute', async () => {
-    const el = await fixture<BiowcHistogram>(
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Custom labels', () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html`<biowc-histogram
+        .xLabel="${HistogramFixture.histogram.xLabel}"
+        .yLabel="${HistogramFixture.histogram.yLabel}"
+      ></biowc-histogram>`
+    );
+  });
+
+  it('can override the x and y labels via attribute', async () => {
+    expect(el.xLabel).to.equal('abundance Gene_X');
+    expect(el.yLabel).to.equal('counts');
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Custom labels', () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html`<biowc-histogram
+        .xLabel="${HistogramFixture.histogram.xLabel}"
+        .yLabel="${HistogramFixture.histogram.yLabel}"
+      ></biowc-histogram>`
+    );
+  });
+
+  it('can override the x and y labels via attribute', async () => {
+    expect(el.xLabel).to.equal('abundance Gene_X');
+    expect(el.yLabel).to.equal('counts');
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Custom size', () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html`<biowc-histogram .height=${200} .width=${300}></biowc-histogram>`
+    );
+  });
+
+  it('can override height and width via attribute', async () => {
+    expect(el.height).to.equal(200);
+    expect(el.width).to.equal(300);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Custom bar color', () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html`<biowc-histogram .barColor="${'#bd282c'}"></biowc-histogram>`
+    );
+  });
+
+  it('can override bar colors', async () => {
+    expect(el.barColor).to.equal('#bd282c');
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Custom data', () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html`<biowc-histogram
+        .xValues=${[{ id: 1, value: 3 }]}
+      ></biowc-histogram>`
+    );
+  });
+
+  it('can override the x values via attribute', async () => {
+    expect(el.xValues.length).to.equal(1);
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Custom number of bins', () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
       html`<biowc-histogram .numBins=${30}></biowc-histogram>`
     );
+  });
 
+  it('can override the number of bins via attribute', async () => {
     expect(el.numBins).to.equal(30);
     // expect(el.bins.length).to.equal(30);
   });
 
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Demo regular histogram', async () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html` <biowc-histogram
+        .idKey="${HistogramFixture.histogram.idKey}"
+        .valueKey="${HistogramFixture.histogram.valueKey}"
+        .xLabel="${HistogramFixture.histogram.xLabel}"
+        .yLabel="${HistogramFixture.histogram.yLabel}"
+        .xValues="${HistogramFixture.histogram.xValues}"
+      />`
+    );
+  });
+
   it('has 3 bars higher than 0', async () => {
     const bars =
-      histogram.shadowRoot!.querySelectorAll<SVGRectElement>(
-        'rect.histogramBar'
-      );
+      el.shadowRoot!.querySelectorAll<SVGRectElement>('rect.histogramBar');
     const visibleBars = Array.from(bars).filter(
       rect => rect.height.baseVal.value > 0
     );
@@ -104,9 +185,7 @@ describe('BiowcHistogram', async () => {
 
   it('has max bar height double the height of the min bar height higher than 0', async () => {
     const bars =
-      histogram.shadowRoot!.querySelectorAll<SVGRectElement>(
-        'rect.histogramBar'
-      );
+      el.shadowRoot!.querySelectorAll<SVGRectElement>('rect.histogramBar');
     const visibleBars = Array.from(bars).filter(
       rect => rect.height.baseVal.value > 0
     );
@@ -118,25 +197,41 @@ describe('BiowcHistogram', async () => {
   });
 
   it('does not show a modal warning box if NaN values are absent', async () => {
-    const modalboxes = histogram.shadowRoot!.querySelectorAll('.modalBox');
+    const modalboxes = el.shadowRoot!.querySelectorAll('.modalBox');
     expect(modalboxes.length).to.equal(0);
   });
 
-  const naHistogram = await fixture<BiowcHistogram>(
-    html` <biowc-histogram
-      .idKey="${HistogramFixture.naHistogram.idKey}"
-      .valueKey="${HistogramFixture.naHistogram.valueKey}"
-      .xLabel="${HistogramFixture.naHistogram.xLabel}"
-      .yLabel="${HistogramFixture.naHistogram.yLabel}"
-      .xValues="${HistogramFixture.naHistogram.xValues}"
-    />`
-  );
+  it('sets the opacity of the tooltip to 0.9 when user hovers over it', async () => {
+    const rect = el.shadowRoot!.querySelector('rect');
+    rect!.dispatchEvent(new MouseEvent('mousemove'));
+    await aTimeout(250);
+    const tooltip = el.shadowRoot!.querySelector('.tooltip') as HTMLElement;
+    expect(tooltip!.style.opacity).to.equal('0.9');
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Demo histogram with NAs', async () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html` <biowc-histogram
+        .idKey="${HistogramFixture.naHistogram.idKey}"
+        .valueKey="${HistogramFixture.naHistogram.valueKey}"
+        .xLabel="${HistogramFixture.naHistogram.xLabel}"
+        .yLabel="${HistogramFixture.naHistogram.yLabel}"
+        .xValues="${HistogramFixture.naHistogram.xValues}"
+      />`
+    );
+  });
 
   it('has 2 bars higher than 0 for naHistogram fixture', async () => {
     const bars =
-      naHistogram.shadowRoot!.querySelectorAll<SVGRectElement>(
-        'rect.histogramBar'
-      );
+      el.shadowRoot!.querySelectorAll<SVGRectElement>('rect.histogramBar');
     const visibleBars = Array.from(bars).filter(
       rect => rect.height.baseVal.value > 0
     );
@@ -145,36 +240,44 @@ describe('BiowcHistogram', async () => {
 
   it('has 3 NaN values for naHistogram fixture', async () => {
     // @ts-ignore
-    const extractedValues = naHistogram._extractValues() as number[]; // accessing private function: https://stackoverflow.com/questions/35987055/how-to-write-unit-testing-for-angular-typescript-for-private-methods-with-jasm
+    const extractedValues = el._extractValues() as number[]; // accessing private function: https://stackoverflow.com/questions/35987055/how-to-write-unit-testing-for-angular-typescript-for-private-methods-with-jasm
     const nanCount = extractedValues.filter(x => Number.isNaN(x)).length;
     expect(nanCount).to.equal(3);
   });
 
   it('shows a modal warning box if NaN values are present', async () => {
-    const modalbox = naHistogram.shadowRoot!.querySelector(
-      '.modalBox'
-    ) as HTMLElement;
+    const modalbox = el.shadowRoot!.querySelector('.modalBox') as HTMLElement;
     expect(modalbox!.style.opacity).to.equal('0.9');
   });
 
-  const kdeHistogram = await fixture<BiowcHistogram>(
-    html` <biowc-histogram
-      .idKey="${HistogramFixture.kdeHistogram.idKey}"
-      .valueKey="${HistogramFixture.kdeHistogram.valueKey}"
-      .xLabel="${HistogramFixture.kdeHistogram.xLabel}"
-      .yLabel="${HistogramFixture.kdeHistogram.yLabel}"
-      .xValues="${HistogramFixture.kdeHistogram.xValues}"
-      .plotKDE="${HistogramFixture.kdeHistogram.plotKDE}"
-    />`
-  );
+  it('passes the a11y audit', async () => {
+    await expect(el).shadowDom.to.be.accessible();
+  });
+});
+
+describe('Demo histogram with KDE', async () => {
+  let el: BiowcHistogram;
+
+  beforeEach(async () => {
+    el = await fixture<BiowcHistogram>(
+      html` <biowc-histogram
+        .idKey="${HistogramFixture.kdeHistogram.idKey}"
+        .valueKey="${HistogramFixture.kdeHistogram.valueKey}"
+        .xLabel="${HistogramFixture.kdeHistogram.xLabel}"
+        .yLabel="${HistogramFixture.kdeHistogram.yLabel}"
+        .xValues="${HistogramFixture.kdeHistogram.xValues}"
+        .plotKDE="${HistogramFixture.kdeHistogram.plotKDE}"
+      />`
+    );
+  });
 
   it('has element path for plotKDE=true', async () => {
-    const paths = kdeHistogram.shadowRoot!.querySelectorAll('.kdePath');
+    const paths = el.shadowRoot!.querySelectorAll('.kdePath');
     expect(paths.length).to.equal(1);
   });
 
   it('has kde path that covers the entire x axis range', async () => {
-    const path = kdeHistogram.shadowRoot!.querySelector('.kdePath');
+    const path = el.shadowRoot!.querySelector('.kdePath');
 
     // for some reason this doesn't work:
     // expect(path!.getBoundingClientRect().width).to.equal("370")
@@ -184,24 +287,11 @@ describe('BiowcHistogram', async () => {
   });
 
   it('renders an invisible tooltip at first', async () => {
-    const tooltip = histogram.shadowRoot!.querySelector(
-      '.tooltip'
-    ) as HTMLElement;
+    const tooltip = el.shadowRoot!.querySelector('.tooltip') as HTMLElement;
     expect(tooltip!.style.opacity).to.equal('0');
   });
 
-  it('sets the opacity of the tooltip to 0.9 when user hovers over it', async () => {
-    const rect = histogram.shadowRoot!.querySelector('rect');
-    rect!.dispatchEvent(new MouseEvent('mousemove'));
-    await aTimeout(250);
-    const tooltip = histogram.shadowRoot!.querySelector(
-      '.tooltip'
-    ) as HTMLElement;
-    expect(tooltip!.style.opacity).to.equal('0.9');
-  });
-
   it('passes the a11y audit', async () => {
-    const el = await fixture(html`<biowc-histogram></biowc-histogram>`);
     await expect(el).shadowDom.to.be.accessible();
   });
 });
